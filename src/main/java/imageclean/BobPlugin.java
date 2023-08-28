0c8dc21e2dd3449c1293095d10093c75e8c6911b
Jurgen Voorneveld <jegvoorneveld@gmail.com>
2023-08-28 22:33:10 +0200

    Added bobplugin file

--- src/main/java/imageclean/BobPlugin.java
+++ src/main/java/imageclean/BobPlugin.java
@@ -3,18 +3,22 @@ package imageclean;
 import bobthebuildtool.pojos.buildfile.Project;
 import bobthebuildtool.pojos.error.VersionTooOld;
 import jcli.errors.InvalidCommandLine;
+import org.apache.commons.imaging.ImageReadException;
+import org.apache.commons.imaging.ImageWriteException;
+import org.apache.commons.imaging.formats.jpeg.exif.ExifRewriter;
 
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.*;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.List;
 import java.util.Map;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 
 import static bobthebuildtool.services.Update.requireBobVersion;
 import static java.nio.file.FileVisitResult.CONTINUE;
+import static java.nio.file.FileVisitResult.TERMINATE;
 import static java.nio.file.Files.isRegularFile;
 import static java.util.stream.Collectors.toList;
 import static jcli.CliParserBuilder.newCliParser;
@@ -38,8 +42,9 @@ public enum BobPlugin {;
         final var writer = new ExifRewriter();
         for (final var path : paths) {
             if (arguments.noRecursion)
-                ;
-            else walkRecursively(path, classes, writer);
+                walkList(path, classes, writer);
+            else
+                walkRecursively(path, classes, writer);
         }
 
         return 0;
@@ -50,31 +55,48 @@ public enum BobPlugin {;
         return List.of(project.parentDir.resolve("src").resolve("main").resolve("images"));
     }
 
-    private static void walkList(final Path path, final Path classes, final ExifRewriter writer) throws IOException {
+    private static void walkList(final Path path, final Path classes, final ExifRewriter writer)
+            throws IOException {
         try (final var list = Files.list(path)) {
-            
-            for (final var file : list) {
-
+            for (final var inFile : list.collect(toList())) {
+                try {
+                    processImage(inFile, classes, path, writer);
+                } catch (final ImageWriteException | ImageReadException e) {
+                    throw new IOException("Failed to remove Exif data from image", e);
+                }
             }
         }
     }
 
-    private static void walkRecursively(final Path path, final Path classes, final ExifRewriter writer) {
+    private static void walkRecursively(final Path path, final Path classes, final ExifRewriter writer) throws IOException {
         Files.walkFileTree(path, new OnlyFileVisitor<>() {
             @Override
-            public FileVisitResult visitFile(Path inFile, BasicFileAttributes attrs) throws IOException {
-                if (!isRegularFile(inFile)) return CONTINUE;
-                if (!inFile.endsWith(".jpg")) return CONTINUE;
-
-                final var outFile = classes.resolve(path.relativize(inFile).toString());
-                try (final var in = Files.newInputStream(inFile);
-                     final var out = Files.newOutputStream(outFile)) {
-                    writer.removeExifMetadata(in, out);
+            public FileVisitResult visitFile(final Path inFile, final BasicFileAttributes attrs) throws IOException {
+                try {
+                    processImage(inFile, classes, path, writer);
+                    return CONTINUE;
+                } catch (final ImageWriteException | ImageReadException e) {
+                    throw new IOException("Failed to remove Exif data from image", e);
                 }
-
-                return CONTINUE;
             }
         });
+    }
+
+    private static void processImage(final Path inFile, final Path classes, final Path root, final ExifRewriter writer)
+                throws ImageWriteException, IOException, ImageReadException {
+        if (!isRegularFile(inFile)) return;
+        if (!inFile.endsWith(".jpg")) return;
+
+        final var outFile = classes.resolve(root.relativize(inFile).toString());
+        removeExifData(inFile, outFile, writer);
+    }
+
+    private static void removeExifData(final Path inFile, final Path outFile, final ExifRewriter writer)
+            throws ImageWriteException, IOException, ImageReadException {
+        try (final var in = Files.newInputStream(inFile);
+             final var out = Files.newOutputStream(outFile)) {
+            writer.removeExifMetadata(in, out);
+        }
     }
 
 }
